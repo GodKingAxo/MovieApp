9f43bd9e97483f2a658f0998b8a0015a1ed2b7ec
King <axovevo@outlook.com>
2021-09-26 10:42:16 -0700

    Many changes ahead
    Created the async function getAPIs() which will now take in the discover api and the genre api. This will be used in order to compare the genre ids from each movie with the id's of each corresponding genre in order to categorize them properly. Kind of gave up on the idea of making multiple pages for each genre.

--- script.js
+++ script.js
@@ -7,25 +7,13 @@ const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280"
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="`
 const GENRE_API = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`
+
+const WITH_GENRE = `https://api.themoviedb.org/3/discover/movie?with_genres=18&api_key=${API_KEY}`
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 const main = document.getElementById('main')
 
-//Get Intial Movies
-getMovies(API_URL);
-getGenres(GENRE_API);
 
-async function getGenres(url) {
-    const res = await fetch(url)
-    const data = await res.json()
-    const genreData = data.genres;
-    genreData.forEach((genre) => {
-        const genreID = genre.id
-        const genreName = genre.name
-        console.log(genreName)
-        //Start creating webpages for each genre
-    })
-}
 function compareGenreID(genre_id, id) {
     
     if(genre_id === id){
@@ -34,37 +22,39 @@ function compareGenreID(genre_id, id) {
         console.log('Not Matching IDs')
     }
 }
-async function getMovies(url) {
-    const res = await fetch(url)
-    const data = await res.json()
-    // console.log(data.results)
-    const movieID = data.results
-    movieID.forEach((id) => {
-        const title = id.title
-        const genreID = id.genre_ids
-        // console.log(title + ' / ' + genreID)
-    })
-    showMovies(data.results)  
+
+//Get Intial Movies
+getAPIS(API_URL, GENRE_API)
+async function getAPIS(discover, genre) {
+    const discoverRes = await fetch(discover)
+    const discoverData = await discoverRes.json()
+
+    const genreRes = await fetch(genre)
+    const genreData = await genreRes.json()
+    showMovies(discoverData.results)
+    console.log(discoverData.results)
+    console.log(genreData.genres)
 }
 function showMovies(movies) {
     main.innerHTML = ''
     movies.forEach((movie) => {
-        const { title, poster_path, vote_average, overview} = movie
+        const { title, poster_path, vote_average, overview, genre_ids} = movie
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
-
+        // console.log(genreIDFilter)
         movieEl.innerHTML = `
         <img src="${IMG_PATH + poster_path}" alt="${title}" />
-        <div class="movie-info">
+        <div class="movie-info" id="movie-info>
           <h3>${title}</h3>
           <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+          <span class="genre-tag"></span>
         </div>
         <div class="overview">
           <h3>Overview</h3>
           ${overview}
         </div>`
       main.appendChild(movieEl)
-    })
+    })  
 }
 
 function getClassByRate(vote) {
