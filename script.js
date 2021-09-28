a703e51d1d7afa91f23097d92fb2a5c0b672e12d
King Axo <axovevo@outlook.com>
2021-09-28 06:40:17 -0700

    More genre tomfoolery
    Got a general direction on what I am trying to accomplish. Am trying to push genre id and name into an array so I can access it easier, but if I can't figure out how to loop and apply properly I might do it manually.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //TODO: Create genre tag. Sort by certain genres
 //When sorting by certain genres I want it to take into account the genre selected and sort it based on popularity descending.
 //The movies have 'genre_ids' tied to them, so I need to create a method that checks what ids a certain movie has and compare it to the base_ids of each genre
-//The main way I can think of going about the genre sorting is by creating different pages for each genre and then implementing the above items.
 const API_KEY = 'ca34b1ab541651378e4fd47bce303f53'
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280"
@@ -25,15 +24,36 @@ function compareGenreID(genre_id, id) {
 
 //Get Intial Movies
 getAPIS(API_URL, GENRE_API)
+
+
 async function getAPIS(discover, genre) {
     const discoverRes = await fetch(discover)
     const discoverData = await discoverRes.json()
 
     const genreRes = await fetch(genre)
     const genreData = await genreRes.json()
+
+
     showMovies(discoverData.results)
-    console.log(discoverData.results)
-    console.log(genreData.genres)
+
+    //TODO: Add a tag  to each movie element with its correct genre. For example: If a movie has the id of "28" then it will have the little tag  "Action"
+    // Similar to how I have the span rating set up. I basically want to do the exact same thing, but apply it to each movie element and make sure they have all the correct genres tied to them
+
+    discoverData.results.forEach((result) => {
+         const movieGenreIDS = result.genre_ids // Gives me a list of genre_ids for each movie on this page. The genre_ids return another array
+        //  console.log(movieGenreIDS) // Looks like the majority of movies have a max of 4 ids, so I would have to set up 4 spans or tags for each genre
+    })
+    
+    const genres = []
+    const genreObj = {} //I think I need to just create an object and not use an array but still not sure
+    genreData.genres.forEach((genre) => {
+        const genreIDS = genre.id
+        const genreName = genre.name
+        // console.log(genreIDS + ' / ' + genreName)
+        genres.push(genreIDS, genreName)
+    }) // Loops through genreData api information and it only pushes each new element into the array, it doesn't make them objects which is what I am trying to do
+
+    console.log(genres)
 }
 function showMovies(movies) {
     main.innerHTML = ''
