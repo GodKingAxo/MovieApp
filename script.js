83e05df341d2af3519da59a209950b3250c4d803
King Axo <axovevo@outlook.com>
2021-09-30 02:02:55 -0700

    Intial commit
    Added section with id and class of pagination and added 5 corresponding buttons into it. Styled it up and now need to do js.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-//TODO: Create genre tag. Sort by certain genres
-//When sorting by certain genres I want it to take into account the genre selected and sort it based on popularity descending.
-//The movies have 'genre_ids' tied to them, so I need to create a method that checks what ids a certain movie has and compare it to the base_ids of each genre
+//With this branch I want to add pagination to this movie app. 
+//Pagination, also known as paging, is the process of dividing a document into discrete pages, either electronic pages or printed pages.
+//We can set some rules on how we want our pagination to be setup.
+//1: 5 pages
+//2: Each to organize based off of popularity descending
+//3: 5 buttons at the bottom of the page.
+
 const API_KEY = 'ca34b1ab541651378e4fd47bce303f53'
-const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1` //Only loads first page
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280"
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="`
 const GENRE_API = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`
@@ -11,7 +15,6 @@ const WITH_GENRE = `https://api.themoviedb.org/3/discover/movie?with_genres=18&a
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 const main = document.getElementById('main')
-
 const genres = [{
     "id": 28,
     "name": "Action" 
@@ -89,7 +92,6 @@ const genres = [{
     "name": "Western"
 },
 ]
-
 //Get Intial Movies
 getAPIS(API_URL, GENRE_API)
 
@@ -139,22 +141,22 @@ function showMovies(movies) {
       if (genre_ids.includes(28)) {
           genreEl.innerHTML = "<span class='genre action'>Action</span>"
       }    else {
-          console.log("N/A")
+        //   console.log("N/A")
       }
       if (genre_ids.includes(28) && genre_ids.includes(12)) {
         genreEl.innerHTML = "<span class='genre action'>Action</span> <span class = 'genre adventure'>Adventure</span>"
     }    else {
-        console.log("N/A")
+        // console.log("N/A")
     }
     if (genre_ids.includes(28) && genre_ids.includes(12) && genre_ids.includes(35)) {
         genreEl.innerHTML = "<span class='genre action'>Action</span> <span class = 'genre adventure'>Adventure</span> <span class = 'genre comedy'>Comedy</span>"
     }    else {
-        console.log("N/A")
+        // console.log("N/A")
     }
     if (genre_ids.includes(28) && genre_ids.includes(12) && genre_ids.includes(35) && genre_ids.includes(14)) {
         genreEl.innerHTML = "<span class='genre action'>Action</span> <span class = 'genre adventure'>Adventure</span> <span class = 'genre comedy'>Comedy</span> <span class = 'genre fantasy'>Fantasy</span>"
     }    else {
-        console.log("N/A")
+        // console.log("N/A")
     }
     //I know for a fact that there is a better way to do this way of adding the genres onto each movie-info div but at this point im kind of done with this for now. Could be improved in the future for sure
       movieEl.appendChild(genreEl)
@@ -169,6 +171,8 @@ function getClassByRate(vote) {
         return "red"
     }
 }
+
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     const searchTerm = search.value
